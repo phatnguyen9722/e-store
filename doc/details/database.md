d17af6dcce37a89f46dc2ce03e09ef05880a1d60
ntanphat1997 <phatnguyen.9722@gmail.com>
2024-05-09 11:08:43 +0700

    [ADD] Model Product

---
+++ doc/details/database.md
@@ -0,0 +1,14 @@
+
+```mermaid
+---
+title: Product
+---
+erDiagram
+    PRODUCT {
+        string title
+        string description
+        float price
+        int inventory
+        datetime last_updated
+    }
+```
--- store/models.py
+++ store/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_updated = models.DateTimeField(auto_now_add=True)
